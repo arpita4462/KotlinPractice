a40f28e3fd880edf4dd0601afa8466878a6d818f
atriodata <info@atrio.co.in>
2017-12-05 15:59:00 +0530

    comit first

--- app/src/main/java/com/learn/atrio/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/learn/atrio/kotlinpractice/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tv_hello.setText("hello kotlin ...")
+        tv_hello.setText("hello kotlin ....")
     }
 }
